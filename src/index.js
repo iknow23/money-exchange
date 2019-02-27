dadc6e2734015292f4567bdfddeaee4bb51bea31
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-02-27 12:15:10 +0300

    add checks to 0/less than 0, and more than 10000

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var result = {};
+    if (currency <= 0) {
+        return result;
+    }
+    if (currency > 10000) {
+        result.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return result;
+    }
 }
